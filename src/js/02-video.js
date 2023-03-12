8707cfc3c8469fcce8bf48e2861afe365f889461
Stanislav-Zhuk <steys.ua@gmail.com>
2023-03-12 16:01:53 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+// get <iframe> video element
+const iframe = document.querySelector('iframe');
+
+// init player with options
+const player = new Player(iframe, {
+  background: true,
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+  responsive: true,
+});
+
+// function to save current video time
+function saveCurrentTime(currentTime) {
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(currentTime.seconds));
+}
+
+// add listen event
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+// set video playback time
+const savedCurrentTime = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0;
+
+player.setCurrentTime(savedCurrentTime);
