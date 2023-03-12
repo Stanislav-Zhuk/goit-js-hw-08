5f6820b02032dc225b59b785387aa5950933cb09
Stanislav-Zhuk <steys.ua@gmail.com>
2023-03-12 15:51:11 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// DOM layout template
+/*
+<a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a> 
+*/
+
+const galleryEl = document.querySelector('.gallery');
+
+// creation gallery layout
+
+const createGalleryMarkup = object => object.map(
+    ({ original, preview, description }) =>
+      `<div class="gallery__item">
+          <a class="gallery__link" href=${original}>
+            <img
+                class="gallery__image"
+                src=${preview}
+                data-source=${original}
+                alt='${description}'
+            />
+          </a>
+      </div>`,
+);
+
+galleryEl.insertAdjacentHTML("afterbegin", createGalleryMarkup(galleryItems).join(""));
+
+// use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250
+});
